888231a4655e831caa527144952d1280883f11cc
Bikal <82023507+Bikal-Adhikari@users.noreply.github.com>
2024-03-25 11:54:08 +1100

    React basic app-Hello world

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <h1>Hello there</h1>
+      <hr />
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id illum alias
+        ea dolor laudantium fugit eum iure culpa sed iusto!
+      </p>
+    </div>
+  );
 }
 
 export default App;
